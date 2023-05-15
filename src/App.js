40c1b24551fbcb3904a241297f3536d48d2df1e9
tonytovan <tonytovan@gmail.com>
2023-05-15 10:03:19 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [light, toggleLight] = React.useState(false);
+    console.log(light);
   return (
-    <main className="off">
+    <main className={light === true ? 'on' : 'off'}>
       <section>
         <div className="dot"></div>
-        <button type="button">Turn on/off</button>
+        <button type="button" onClick={() => toggleLight(!light)}>
+            {light === true ? 'Turn off': 'Turn on'}
+        </button>
       </section>
     </main>
   );
